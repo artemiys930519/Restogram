2969ad0cc689e5d097d5c9ce7865279fb8d488cb
artemiys930519 <artemiys930519@yandex.ru>
2018-12-16 23:25:22 +0500

    replacing junit on assertj

--- src/test/java/app/repository/JdbcRestaurantRepositoryTest.java
+++ src/test/java/app/repository/JdbcRestaurantRepositoryTest.java
@@ -1,41 +1,32 @@
 package app.repository;
 
 
+import app.TimingExtension;
 import app.model.Restaurant;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.Stopwatch;
-import org.junit.runner.Description;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static app.RestaurantTestData.*;
 
 @ContextConfiguration(locations = {"classpath:spring/spring-app.xml",
         "classpath:spring/spring-db.xml"})
+
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith({SpringExtension.class,TimingExtension.class})
 @ActiveProfiles("jdbc")
+
 public class JdbcRestaurantRepositoryTest {
 
     @Autowired
     RestaurantRepository repository;
-
-    @Rule
-    public Stopwatch watch = new Stopwatch() {
-        @Override
-        protected void finished(long nanos, Description description) {
-            System.out.println(String.format("%-20s %d",description.getMethodName(),TimeUnit.NANOSECONDS.toMillis(nanos)));
-        }
-    };
 
     @Test
     public void getTest() {
@@ -45,22 +36,23 @@ public class JdbcRestaurantRepositoryTest {
     @Test
     public void deleteTest() {
         repository.delete(RESTAURANT1.getId());
-        assertMatch(List.of(RESTAURANT2,RESTAURANT3,RESTAURANT4,RESTAURANT5),RESTAURANT2,RESTAURANT3,RESTAURANT4,RESTAURANT5);
+        assertMatch(List.of(RESTAURANT2, RESTAURANT3, RESTAURANT4, RESTAURANT5), RESTAURANT2, RESTAURANT3, RESTAURANT4, RESTAURANT5);
     }
 
     @Test
     public void getAllTest() {
-        assertMatch(repository.getAll(),restaurants);
+        assertMatch(repository.getAll(), restaurants);
     }
 
     @Test
     public void saveTest() {
         Restaurant result = repository.save(getCreated());
-        assertMatch(repository.get(result.getId()),result);
+        assertMatch(repository.get(result.getId()), result);
     }
+
     @Test
     public void updateTest() {
         Restaurant result = repository.save(getUpdated());
-        assertMatch(repository.get(result.getId()),result);
+        assertMatch(repository.get(result.getId()), result);
     }
 }
